823d98c24911edf7a77945de755f143e3c35c083
LodeNik23 <140551123+LodeNik23@users.noreply.github.com>
2023-11-14 19:48:52 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,14 +5,14 @@ const allcards = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 
 const createCard = (cards, deleteCallback) => {
-    const cardN = cardtemplate.querySelector('.card').cloneNode(true);
-    const cardsImg = cardN.querySelector('.card__image');
-    const cardsTtl = cardN.querySelector('.card__title');
+    const cardItem = cardtemplate.querySelector('.card').cloneNode(true);
+    const cardsImg = cardItem.querySelector('.card__image');
+    const cardsTtl = cardItem.querySelector('.card__title');
     cardsImg.src = cards.link;
     cardsImg.alt = cards.name; 
     cardsTtl.textContent = cards.name;
-    cardN.querySelector('.card__delete-button').addEventListener('click', deleteCallback);
-    return cardN;
+    cardItem.querySelector('.card__delete-button').addEventListener('click', deleteCallback);
+    return cardItem;
 };
 // @todo: Функция удаления карточки
 const deleteCallback = (event) => {
