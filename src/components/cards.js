b12cb50df325e33bc798923e9e8616250ae7004f
NikitaBrin <NikitaBrin23@yandex.ru>
2023-12-10 23:32:15 +0300

    its alive but

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,55 +1,61 @@
-import arhiz from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
-import cheliabinsk from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg';
-import ivanovo from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg';
-import kamchatka from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg';
-import holmogorsky from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg';
-import baikal from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg';
-
 const initialCards = [
     {
       name: "Архыз",
-      link: arhiz,
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
     },
     {
       name: "Челябинская область",
-      link:  cheliabinsk,
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
     },
     {
       name: "Иваново",
-      link: ivanovo,
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
     },
     {
       name: "Камчатка",
-      link: kamchatka,
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
     },
     {
       name: "Холмогорский район",
-      link: holmogorsky,
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
     },
     {
       name: "Байкал",
-      link: baikal,
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
     }
 ];
 // @todo: Функция создания карточки
-
-const createCard = (cards, deleteCallback) => {
-
-  const cardItem = cardtemplate.querySelector('.card').cloneNode(true);
-  const cardsImg = cardItem.querySelector('.card__image');
-  const cardsTtl = cardItem.querySelector('.card__title');
-
-  cardsImg.src = cards.link;
-  cardsImg.alt = cards.name; 
-  cardsTtl.textContent = cards.name;
-
-  cardItem.querySelector('.card__delete-button').addEventListener('click', deleteCallback);
+/*при включении в функию 'likeCard, deleteCallback' - ломается работа*/ 
+function createCard(cards, openImageClick) {
+  const cardtemplate = document.querySelector("#card-template").content,
+    cardItem = cardtemplate.querySelector(".card").cloneNode(true),
+    cardImg = cardItem.querySelector(".card__image"),
+    cardTtl = cardItem.querySelector(".card__title"),
+    likeBtn = cardItem.querySelector(".card__like-button"),
+    deleteBtn = cardItem.querySelector(".card__delete-button");
+  cardImg.src = cards.link;
+  cardImg.alt = cards.name;
+  cardTtl.textContent = cards.name;
+  cardImg.addEventListener("click", openImageClick);
+   deleteBtn.addEventListener("click", deleteCallback);
+  /* likeBtn.addEventListener("click", likeCard); если раскомитить - перестает работать*/
   
   return cardItem;
-};
+}
+
+// @todo: Функция лайка карточки
+
+const likeCard = (evt) => {
+  if (evt.target.classList.contains('card__like-button')) {
+   evt.target.classList.toggle('card__like-button_is-active'); 
+  // console.log("likeCard")
+  };
+}; 
 
 // @todo: Функция удаления карточки
-const deleteCallback = (event) => {
-  event.target.closest('.card').remove();
+const deleteCallback = (evt) => {
+  evt.target.closest('.card').remove();
+//  console.log("likeCard");
 }
-export { initialCards, createCard, deleteCallback };
+
+export { initialCards, createCard, deleteCallback, likeCard};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
 import "./pages/index.css";
-import {
-    initialCards,
-    createCard,
-    deleteCallback,
-  } from "./components/cards";
+import {openPopup, closePopup, closeEsc} from "./components/popups";
+import {initialCards, createCard, deleteCallback, likeCard} from "./components/cards";
 
 // @todo: Темплейт карточки
-const cardtemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
 const allcards = document.querySelector('.places__list');
 
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const profileTitle = profile.querySelector('.profile__title');
+const profileDescription = profile.querySelector('.profile__description');
+const profileAddButton = profile.querySelector('.profile__add-button');
+
+const profileForm = document.querySelector('.popup__form[name="edit-profile"]');
+const nameInput = profileForm.querySelector('.popup__input_type_name');
+const jobInput = profileForm.querySelector('.popup__input_type_description');
+
+const cardForm = document.querySelector('.popup__form[name="new-place"]');
+const cardNameInput = cardForm.querySelector('.popup__input_type_card-name');
+const cardLinkInput = cardForm.querySelector('.popup__input_type_url');
+
+//ПОПАПЫ
+const popups = document.querySelectorAll(".popup"),
+  popupEditProfile = document.querySelector(".popup_type_edit"),
+  popupNewCard = document.querySelector(".popup_type_new-card"),
+  popupImageBox = document.querySelector(".popup_type_image"),
+  popupImage = popupImageBox.querySelector(".popup__image"),
+  popupCaption = popupImageBox.querySelector(".popup__caption");
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardItm) => {
-    allcards.append(createCard(cardItm, deleteCallback));
+    allcards.append(createCard(cardItm, openImageClick, deleteCallback));
 });
+
+ //Данные профиля 
+function handleFormSubmitProfile(evt){
+  evt.preventDefault();  
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  closePopup(popupEditProfile);
+};
+
+//Добавить карточку
+
+function handleFormSubmitNewCard(evt){
+  evt.preventDefault();  
+  const newCard = createCard(
+    {name:cardNameInput.value, link: cardLinkInput.value}, openImageClick, likeCard, deleteCallback);
+    allcards.prepend(newCard);
+  closePopup(popupNewCard);
+  cardForm.reset();
+};
+
+// cardImage.addEventListener('click', (evt) => openImageClick(evt.target.alt, evt.target.src));
+
+//Увеличить изображение
+function openImageClick(evt) {
+  const card = evt.target.closest('.card'),
+  cardImage = card.querySelector('.card__image'),
+  cardTitle = card.querySelector('.card__title');
+
+  popupImage.src = cardImage.src;
+  popupImage.alt = cardTitle.alt;
+  popupCaption.textContent = cardTitle.textContent;
+
+  openPopup(popupImageBox);
+}
+
+//Обработчики
+
+profileForm.addEventListener('submit', handleFormSubmitProfile);
+cardForm.addEventListener('submit', handleFormSubmitNewCard);
+profileEditButton.addEventListener('click',() => {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+openPopup(popupEditProfile);
+});
+profileAddButton.addEventListener('click',() => openPopup(popupNewCard));
+popups.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+    if (evt.target.matches('.popup_is-opened, .popup__close'))
+      closePopup(popup);
+  });
+});
+
+allcards.addEventListener('click', likeCard);
+
+export {allcards};
